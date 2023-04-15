10b34189573dffc76818f72be1ec1b2713a72536
Jason Shi <jshigoodies@gmail.com>
2023-04-15 16:30:17 -0500

    adds to database called jate

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -13,9 +13,27 @@ const initdb = async () =>
   });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+
+//literally copying from student indexDB
+export const putDb = async (content) => {
+  const todoDB = await initdb();
+  const tx = todoDB.transaction('jate', 'readwrite'); //permissions
+  const store = tx.objectStore('jate'); //access the storage in 'jate' database
+  await store.put({content});
+  await tx.done;
+  console.log('Content added to database');
+
+};
 
 // TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+export const getDb = async () => {
+  const db = await initdb();
+  const tx = db.transaction('jate', 'readonly');
+  const store = tx.objectStore('jate');
+  const contents = await store.getAll();
+  await tx.done;
+  console.log('Content retrieved from database successfully');
+  return contents;
+};
 
 initdb();
--- client/src/js/install.js
+++ client/src/js/install.js
@@ -1,11 +1,40 @@
 const butInstall = document.getElementById('buttonInstall');
+let deferredPrompt;
 
 // Logic for installing the PWA
 // TODO: Add an event handler to the `beforeinstallprompt` event
-window.addEventListener('beforeinstallprompt', (event) => {});
+window.addEventListener('beforeinstallprompt', (event) => {
+  // Prevent Chrome 67 and earlier from automatically showing the prompt
+  event.preventDefault();
+
+  // Stash the event so it can be triggered later
+  deferredPrompt = event;
+
+  // Update UI to notify the user that they can add the app to the home screen
+  butInstall.style.display = 'block';
+});
 
 // TODO: Implement a click event handler on the `butInstall` element
-butInstall.addEventListener('click', async () => {});
+butInstall.addEventListener('click', async () => {
+  // Show the prompt
+  deferredPrompt.prompt();
+
+  // Wait for the user to respond to the prompt
+  const { outcome } = await deferredPrompt.userChoice;
+
+  // Update UI to notify the user whether they installed the app or not
+  if (outcome === 'accepted') {
+    console.log('User accepted the install prompt');
+  } else {
+    console.log('User dismissed the install prompt');
+  }
+
+  // Clear the deferredPrompt variable
+  deferredPrompt = null;
+});
 
 // TODO: Add an handler for the `appinstalled` event
-window.addEventListener('appinstalled', (event) => {});
+window.addEventListener('appinstalled', (event) => {
+  // Show a confirmation message to the user
+  console.log('App installed successfully!');
+});
